78a9d669dd2a997e4618a21f83aa9cea0656f3ff
Krzysztof Sztompka <sztompka.krzysztof@gmail.com>
2017-05-09 23:39:54 +0200

    manager tests

--- tests/manager.js
+++ tests/manager.js
@@ -12,27 +12,62 @@ describe("Manager", ()=>{
     expect(manager).to.be.an.instanceof(Manager);
   })
 
-  describe("for existing manager instance", () => {
+  describe("when manager instance exist", () => {
     var manager;
 
     beforeEach(() => {
       manager = new Manager();
     });
 
-    it("should return empty extension list when no extensions registered", () => {
+    it("should return empty extension list", () => {
       expect(manager.getExtensions().length).to.be.equal(0);
     });
 
     it("should register and init extension", () => {
-      const extensionStub = {
-        init: sinon.stub(),
-        getName: sinon.stub().returns("SOME_NAME")
-      };
+      const extensionStub = createExtensionStub("SOME_NAME");
       manager.registerExtension(extensionStub);
       expect(extensionStub.getName.called).be.false;
       expect(extensionStub.init.called).be.true;
-    })
+      expect(manager._extensions[0]).be.equal(extensionStub);
+    });
+
+    describe("when registered extension", () => {
+      var extensionStub;
+      beforeEach(() => {
+        extensionStub = createExtensionStub("SOME_NAME");
+        manager.registerExtension(extensionStub);
+      });
+
+      it("should return extension list", () => {
+        expect(manager.getExtensions().length).to.be.equal(1);
+        expect(manager.getExtensions()[0]).to.be.equal(extensionStub);
+      });
+    });
+
+    it("should register event listener", () => {
+      const callbackStub = sinon.stub();
+      manager.registerEventListener("SOME_NAME", callbackStub);
+      expect(manager._events[0]._callbacks[0]).be.equal(callbackStub);
+    });
+
+    describe("when registered event listener", () => {
+      var callbackStub;
+      beforeEach(() => {
+        callbackStub = sinon.stub();
+        manager.registerEventListener("SOME_NAME", callbackStub);
+      });
+
+      it("should call callback on event call", () => {
+        manager.callEvent("SOME_NAME", "SOME_VALUE");
+        expect(callbackStub.called).be.true;
+      })
+    });
   });
 });
 
-
+function createExtensionStub(extensionName, init = sinon.stub()) {
+  return {
+    init,
+    getName: sinon.stub().returns(extensionName)
+  };
+}
