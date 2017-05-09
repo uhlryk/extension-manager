48eb0b27cf0e0453e123488b9bb279d46e007b82
Krzysztof Sztompka <sztompka.krzysztof@gmail.com>
2017-05-09 23:13:34 +0200

    update existing tests

--- tests/manager.js
+++ tests/manager.js
@@ -1,31 +1,38 @@
 import chai from "chai";
-var expect = chai.expect;
+const expect = chai.expect;
 import sinon from "sinon";
 
 import Manager from "../src/Manager";
 
 describe("Manager", ()=>{
-  var manager;
+
 
   it("should create manager instance", () => {
-    manager = new Manager();
+    const manager = new Manager();
     expect(manager).to.be.an.instanceof(Manager);
   })
-;
-  it("should return empty extension list when no extensions registered", () => {
-    expect(manager.getExtensions().length).to.be.equal(0);
+
+  describe("for existing manager instance", () => {
+    var manager;
+
+    beforeEach(() => {
+      manager = new Manager();
+    });
+
+    it("should return empty extension list when no extensions registered", () => {
+      expect(manager.getExtensions().length).to.be.equal(0);
+    });
+
+    it("should register and init extension", () => {
+      const extensionStub = {
+        init: sinon.stub(),
+        getName: sinon.stub().returns("SOME_NAME")
+      };
+      manager.registerExtension(extensionStub);
+      expect(extensionStub.getName.called).be.false;
+      expect(extensionStub.init.called).be.true;
+    })
   });
-
-  it("should register and init extension", () => {
-    const extensionStub = {
-      init: sinon.stub(),
-      getName: sinon.stub().returns("SOME_NAME")
-    };
-    manager.registerExtension(extensionStub);
-    expect(extensionStub.getName.called).be.false;
-    expect(extensionStub.init.called).be.true;
-  })
-
 });
 
 
